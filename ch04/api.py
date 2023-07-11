5c7f610ae829a534b275dbc68f90e127d7a5e65c
Shubham <shub655@gmail.com>
2023-07-11 16:59:15 +0800

    html changes

--- ch04/api.py
+++ ch04/api.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from ch04.todo import todo_router
-
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(
     title= "Traincote Api : CH04",
     description= "This is Learning API for chapter 4"
 )
 
+app.mount('/static', StaticFiles(directory='./ch04/static'), name='static')
 app.include_router(todo_router)
