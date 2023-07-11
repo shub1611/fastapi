4c07b568f4a2b22702c1c1435e80c389aa0b9dc4
Shubham <shub655@gmail.com>
2023-07-11 16:24:13 +0800

    ok

--- ch04/todo.py
+++ ch04/todo.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Form, Request
 from fastapi.templating import Jinja2Templates
 
 todo_router = APIRouter(tags=["ToDo-HTML"])
-todo_list = []
+todo_list = [{"title": 1}, {"title":2}]
 
 template = Jinja2Templates(directory='./ch04/templates')
 
@@ -10,7 +10,16 @@ template = Jinja2Templates(directory='./ch04/templates')
 async def add_todo(request: Request):
     pass
 
-
 @todo_router.get('/todo')
 async def get_todo(request: Request):
     return template.TemplateResponse('todo.html', {'request': request, 'todos': todo_list})
+
+@todo_router.get('/todo-create')
+async def get_todo(request: Request):
+    return template.TemplateResponse('todo_form.html', {'request': request})
+
+@todo_router.post("/todo-save")
+async def save(request: Request, item: str = Form(...)):
+    print('item =', item)
+    todo_list.append({"title": item})
+    return 'ok'
