73d053d5859207e357bd11b8232d4e235a74b2fc
Shubham <shub655@gmail.com>
2023-07-13 14:17:32 +0800

    authentication and authorization

---
+++ ch07-auth-jwt/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+import uvicorn
+from routes.users import user_router
+from routes.events import event_router
+from database.connection import Settings
+
+app = FastAPI(
+    title= "Traincote Api : CH06-MongoDb",
+    description= "FastAPI SQLModel learning in chapter 6"
+)
+
+
+app.include_router(user_router, prefix="/user")
+app.include_router(event_router, prefix="/events")
+
+setting = Settings()
+
+# this will open connection to DB and create tables
+@app.on_event("startup")
+async def on_startup():
+    await setting.initialize_database()
+
+
+# @app.get("/")
+# async def home():
+#     return RedirectResponse(url='/event')
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
